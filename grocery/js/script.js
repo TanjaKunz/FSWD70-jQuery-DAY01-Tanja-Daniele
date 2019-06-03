7dacb186490b539641000755a462c8c467b2124f
Daniele Argiolas <danieleargiolas9@gmail.com>
2019-06-03 15:26:48 +0200

    2

--- grocery/js/script.js
+++ grocery/js/script.js
@@ -5,8 +5,12 @@ $(document).ready(function() {
 	for (let i = 0; i < arr.length; i++) {				
 
 		$(arr[i]).attr("src", items[i].pic);
-		$(name[i]).text(items[i].name).after("<div> € "+items[i].price+"</div");
+		$(name[i]).text(items[i].name).after("<div> € "+items[i].price+"</div>");
 		$(".items1 div div").attr("class", "price");
+		$("#basket").append("<p class='myp'>"+items[i].name+" "+items[i].amount+"</p>");
 	
 	}
-});
+
+
+		});
+
